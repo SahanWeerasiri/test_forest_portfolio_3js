bbbef16b2a9ce7f69cb53a6d924e1d50a30a3e13
sahanw-enigmalk <sahanw@enigmasolutionslk.com>
2026-03-05 11:06:44 +0530

    Update production workflow to remove deploy_hub trigger and set Router basename in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 const App = () => {
     return (
         <main className="bg-white">
-            <Router>
+            <Router basename="/portfolio/">
                 <Routes>
                     <Route path='/' element={<Home />} />
                 </Routes>
